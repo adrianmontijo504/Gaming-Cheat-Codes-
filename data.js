52d36d6180d51156646ada3e2b69f3996318026c
adrianmontijo504 <adrianmontijo319@gmail.com>
2026-04-03 09:41:35 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+window.SYSTEMS = [
+  {
+    name: "PlayStation 5",
+    description: "Browse PlayStation 5 cheat guides.",
+    href: "ps5.html",
+    status: "Live"
+  },
+  {
+    name: "Xbox Series X|S",
+    description: "Add Xbox games here later.",
+    href: "#",
+    status: "Coming Soon"
+  },
+  {
+    name: "PC",
+    description: "Add PC games here later.",
+    href: "#",
+    status: "Coming Soon"
+  },
+  {
+    name: "Nintendo Switch",
+    description: "Add Nintendo Switch games here later.",
+    href: "#",
+    status: "Coming Soon"
+  }
+];
+
+window.GAMES_BY_SYSTEM = {
+  "PlayStation 5": [
+    {
+      name: "The Sims 4",
+      description: "Open the PlayStation 5 Sims 4 page.",
+      href: "sims4-ps5.html",
+      status: "Live"
+    }
+  ]
+};
